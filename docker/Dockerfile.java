fcdb94443147bd2516bdfc14d6c7e2f5e407caf8
Matt Owen <matt.owen@lazarus.enterprises>
2026-07-29 15:16:00 -0400

    :hammer_and_wrench:  fix docker

--- docker/Dockerfile.java
+++ docker/Dockerfile.java
@@ -3,7 +3,7 @@ FROM eclipse-temurin:21
 RUN apt-get update -qq && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y -qq \
         maven ripgrep ca-certificates \
-        python3 python3-pip valgrind ccache git ltrace && \
+        python3 python3-pip valgrind ccache git && \
     rm -rf /var/lib/apt/lists/*
 
 RUN pip3 install --break-system-packages pyjwt requests cryptography pycryptodome
